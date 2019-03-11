4d598a1c7c7fc8cce7c351a33adfd276cd9d3475
Atanas Kaynarov <akaynarov@gmail.com>
2019-03-11 17:43:02 +0200

    Add sign in page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 
-export const publicRoutes = [{ path: "/", component: Home, exact: true }];
+export const publicRoutes = [
+  { path: "/", component: Home, exact: true },
+  { path: "/sign-in", component: SignIn, exact: true }
+];
 
 export const privateRoutes = [
   {
