228376789baba11eb844b2c5c6ef85ebe0731e2a
Atanas Kaynarov <akaynarov@gmail.com>
2019-03-15 15:10:21 +0200

    Add navbar

--- src/components/MainRoutes.js
+++ src/components/MainRoutes.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import { publicRoutes, privateRoutes } from "../routes";
 import RouteWithSubRoutes from "./RouteWithSubRoutes";
 import PrivateRoute from "./PrivateRoute";
+import Navbar from "./Navbar";
 
 const NotFound = () => <h1>Not Found</h1>;
 
 const MainRoutes = () => (
-  <Switch>
-    {publicRoutes.map(route => (
-      <RouteWithSubRoutes key={route.path} {...route} />
-    ))}
-    {privateRoutes.map(route => (
-      <PrivateRoute key={route.path} {...route} />
-    ))}
-    <Route path="/*" component={NotFound} />
-  </Switch>
+  <Fragment>
+    <Navbar />
+    <Switch>
+      {publicRoutes.map(route => (
+        <RouteWithSubRoutes key={route.path} {...route} />
+      ))}
+      {privateRoutes.map(route => (
+        <PrivateRoute key={route.path} {...route} />
+      ))}
+      <Route path="/*" component={NotFound} />
+    </Switch>
+  </Fragment>
 );
 
 export default MainRoutes;
